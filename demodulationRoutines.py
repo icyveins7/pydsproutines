1d18aaf9a0b3692911a1a8aeb718093b563b9a55
icyveins7 <icyveins@hotmail.com>
2020-07-17 20:02:42 +0800

    add maxlikelihood qpsk demod

--- demodulationRoutines.py
+++ demodulationRoutines.py
@@ -28,3 +28,74 @@ def demodulateCP2FSK(syms, h, up, sIdx):
         demodBits[i] = np.argmax(bitCost[:,i])
         
     return demodBits, bitCost, tones
+
+
+def convertIntToBase4Combination(l, i):
+    base_4_repr = np.array(list(np.base_repr(i,base=4)),dtype=np.uint8)
+    base_4_repr = np.pad(base_4_repr, (l - len(base_4_repr),0)) # pad it to the numSyms
+    
+    return base_4_repr
+
+def ML_demod_QPSK(y, h, up, numSyms):
+    '''
+    Brute force search over all combinations, don't use this if you have a long string of symbols.
+    '''
+    
+    totalCombi = 4**numSyms
+    cost = np.zeros(totalCombi)
+    
+    for i in range(totalCombi):
+        base_4_repr = convertIntToBase4Combination(numSyms, i)
+
+        qpsk_syms = np.exp(1j*base_4_repr*np.pi/2)
+        
+        qpsk_syms_up = np.zeros(numSyms * up, dtype=np.complex128)
+        qpsk_syms_up[::up] = qpsk_syms
+        
+        # convolve this test symbol set with the channel
+        test = np.convolve(h, qpsk_syms_up)
+        
+        # find the normalised dot product
+        test = test[up : up + len(y)]
+        test_cost = np.vdot(y, test) / np.linalg.norm(y) / np.linalg.norm(test)
+        cost[i] = np.abs(test_cost)**2.0
+    
+        
+        # print(base_4_repr)
+        # print(qpsk_syms_up)
+        # print(test)
+        
+        # if np.all(base_4_repr == np.array([0,1,2,1,2,3])):
+        #     print(base_4_repr)
+        #     print(test)
+        #     print(qpsk_syms_up)
+        #     break
+        
+    ii = np.argmax(cost)
+    mm = convertIntToBase4Combination(numSyms, ii) 
+    
+    
+    return mm, ii, cost
+            
+
+# def ML_demod_CPM_laurent(y, h, up, numSyms, outputCost=False):
+#     '''
+#     Brute force search over all combinations, don't use this if you have a long string of symbols.
+#     '''
+    
+#     totalCombi = 2**numSyms
+#     cost = np.zeros(totalCombi)
+#     rowsToKeep = int(np.ceil(numSyms)/8)
+    
+#     for i in range(totalCombi):
+#         i_arr = np.array([i], np.uint32) # convert to array
+#         i_arr = i_arr.view(np.uint8).reshape((-1,1)) # convert to byte level
+#         i_bits = np.unpackbits(i_arr, axis=1, bitorder='little')
+        
+#         i_bits = i_bits[:rowsToKeep+1].flatten()
+#         i_bits = i_bits[0:numSyms]
+        
+#         print(i_bits)
+        
+#         for b in range(len(i_bits)):
+            

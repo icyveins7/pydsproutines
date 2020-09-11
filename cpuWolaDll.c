cdb966799b3b7c58c9e525b2a35fa4e6a9b168d3
icyveins7 <icyveins@hotmail.com>
2020-09-11 21:13:39 +0800

    edit base cpuWola to use IPP instead, disabled the rest
    current method includes the NUM_THREADS option parameter, and only allows up to Dec or 2*Dec for fftlen like the mex version

--- cpuWolaDll.c
+++ cpuWolaDll.c
@@ -1,16 +1,14 @@
+// cl /O2 /LD cpuWolaDll.c ippcore.lib ipps.lib
+
 #include <math.h>
 #include "stdio.h"
 #include <stdlib.h>
-#include "fftw3.h"
+#include "ipp.h"
+#include <time.h>
 #include <string.h>
-#include <ipp.h>
-
-// #include <pthread.h>
-
 #include <windows.h>
 #include <process.h>
 
-#define NUM_THREADS 22
 #define DLL_EXPORT __declspec(dllexport)
 
 #ifdef __cplusplus
@@ -20,126 +18,117 @@ extern "C" {
 // definition of thread data
 struct thread_data{
 	int thread_t_ID;
+	int thread_NUMTHREADS;
 	
-	fftw_complex *thread_y;
-	double *thread_f_tap;
+	Ipp32fc *thread_y;
+	float *thread_f_tap;
 	int thread_L;
 	int thread_N;
 	int thread_Dec;
 	int thread_nprimePts;
 	
-	fftw_complex *thread_fin;
-	fftw_complex *thread_fout;
+	// IPP DFT vars
+	Ipp8u *thread_pDFTBuffer;
+	IppsDFTSpec_C_32fc *thread_pDFTSpec;
 
-	Ipp64fc *thread_tones;
-	
-	fftw_complex *thread_out;
+	Ipp32fc *thread_out; // for R2018
 };
 
-// declare global thread stuff
-struct thread_data thread_data_array[NUM_THREADS];
-
-// test fftw_plans array on stack for threads, works
-fftw_plan allplans[NUM_THREADS]; // REMEMBER TO CHECK FFTW PLANS CREATION IN THE ENTRY FUNCTION
 
 unsigned __stdcall threaded_wola(void *pArgs){
-// void *threaded_wola(void *pArgs){
     struct thread_data *inner_data;
 	inner_data = (struct thread_data *)pArgs;
 	
 	int t_ID = inner_data->thread_t_ID;
-	fftw_complex *y = inner_data->thread_y;
+	int NUM_THREADS = inner_data->thread_NUMTHREADS;
+	
+	Ipp32fc *y = inner_data->thread_y;
 	int L = inner_data->thread_L;
 	int N = inner_data->thread_N;
 	int Dec = inner_data->thread_Dec;
 	int nprimePts = inner_data->thread_nprimePts;
-	double *f_tap = inner_data->thread_f_tap;
+	float *f_tap = inner_data->thread_f_tap;
 
-	fftw_complex *fin = inner_data->thread_fin;
-	fftw_complex *fout = inner_data->thread_fout;
+	// IPP DFT vars
+	Ipp8u *pDFTBuffer = inner_data->thread_pDFTBuffer;
+	IppsDFTSpec_C_32fc *pDFTSpec = inner_data->thread_pDFTSpec;
 
-	Ipp64fc *tones = inner_data->thread_tones;
-	
-	fftw_complex *out = inner_data->thread_out; // for R2018
+	Ipp32fc *out = inner_data->thread_out; // for R2018
 	// end of assignments
     
     int nprime, n, a, b; // declare to simulate threads later
     int k;
-	int tone_idx;
+	
+	// allocate for FFTs
+	Ipp32fc *dft_in = (Ipp32fc*)ippsMalloc_32fc_L(N);
+	Ipp32fc *dft_out = (Ipp32fc*)ippsMalloc_32fc_L(N);
 
 	// pick point based on thread number
 
 	for (nprime = t_ID; nprime<nprimePts; nprime=nprime+NUM_THREADS){
         n = nprime*Dec;
+		
+		ippsZero_32fc(dft_in, N);
+		
         for (a = 0; a<N; a++){
-            fin[a][0] = 0; // init to 0
-            fin[a][1] = 0;
             for (b = 0; b<L/N; b++){
                 if (n - (b*N+a) >= 0){
-                    fin[a][0] = fin[a][0] + y[n-(b*N+a)][0] * f_tap[b*N+a];
-                    fin[a][1] = fin[a][1] + y[n-(b*N+a)][1] * f_tap[b*N+a];
-                } // fin is fftw_complex
+					dft_in[a].re = dft_in[a].re + y[n-(b*N+a)].re * f_tap[b*N+a];
+					dft_in[a].im = dft_in[a].im + y[n-(b*N+a)].im * f_tap[b*N+a];
+                } 
             }
         }
-        fftw_execute(allplans[t_ID]); // this should place them into another fftw_complex fout
+		
+		// ippsDFTInv_CToC_32fc(dft_in, dft_out, pDFTSpec, pDFTBuffer); // actually you can write directly to the matlab output in r2018 since it's interleaved
+		ippsDFTInv_CToC_32fc(dft_in, (Ipp32fc*)&out[nprime*N], pDFTSpec, pDFTBuffer);
+		
+
         
+		if (Dec*2 == N && nprime % 2 != 0){ // only if using overlapping channels, do some phase corrections when nprime is odd
+			for (k=1; k<N; k=k+2){ //  all even k are definitely even in the product anyway
+				// dft_out[k].real = -dft_out[k].real;
+				// dft_out[k].imag = -dft_out[k].imag; // actually you can write directly to the matlab output in r2018 since it's interleaved
+				out[nprime*N + k].re = -out[nprime*N + k].re;
+				out[nprime*N + k].im = -out[nprime*N + k].im;
+			}
+		}
 		
-		// // === old code for up to bin overlap of 2 ===
-		// if (Dec*2 == N && nprime % 2 != 0){ // only if using overlapping channels, do some phase corrections when nprime is odd
-			// for (k=1; k<N; k=k+2){ //  all even k are definitely even in the product anyway
-				// fout[k][0] = -fout[k][0];
-				// fout[k][1] = -fout[k][1];
-			// }
-		// }
-		
-        // memcpy(&out[nprime*N],fout,sizeof(fftw_complex)*N);
-		
-		// === new code for general bin overlaps ===
-		tone_idx = nprime % (N/Dec);
-		ippsMul_64fc((Ipp64fc*)fout, &tones[tone_idx * N], (Ipp64fc*)&out[nprime*N], N);
+        // memcpy(&out[nprime*N],fout,sizeof(Ipp32fc)*N); // if you write directly, you won't need to copy it
 	}
+	
+	ippsFree(dft_in);
+	ippsFree(dft_out);
 	
 	_endthreadex(0);
     return 0;
 }
 
-extern DLL_EXPORT int cpuWola(fftw_complex *y, double *f_tap, int fftlen, int Dec, int nprimePts, int L, fftw_complex *out){
-// int cpuWola(fftw_complex *y, double *f_tap, int fftlen, int Dec, int nprimePts, int L, fftw_complex *out){
+/* The gateway function */
+extern DLL_EXPORT int cpuWola(Ipp32fc *y, Ipp32f *f_tap, int fftlen, int Dec, int nprimePts, int L, Ipp32fc *out, int NUM_THREADS){
 	ippInit();
-	Ipp64fc *tones;
-	Ipp64f phase = 0;
-	Ipp64f rFreq = 0;
-
-	fftw_complex *fin, *fout;
-	int i, t;
+    // declare variables
+    int t; // for loops over threads
+    HANDLE *ThreadList = (HANDLE*)malloc(sizeof(HANDLE) * NUM_THREADS);// handles to threads
+	struct thread_data *thread_data_array = (struct thread_data *)malloc(sizeof(struct thread_data) * NUM_THREADS);
 	
-	tones = ippsMalloc_64fc_L(fftlen/Dec * fftlen); // the tone is fftlen elements, and we need fftlen/Dec of them before the phase correction repeats
-	
-	for (i=0; i<fftlen/Dec; i++){
-		rFreq = -(Ipp64f)i * (Ipp64f)Dec / (Ipp64f)fftlen;
-		if (rFreq<0){rFreq = rFreq + 1;} // basically other than the first one, all are negative, so shift to the positive equivalent
-		ippsTone_64fc(&tones[i*fftlen], fftlen, 1.0, rFreq, &phase, ippAlgHintNone);
+    // ====== ALLOC VARS FOR FFT IN THREADS BEFORE PLANS ====================
+	// ===== IPP DFT Allocations =====
+	int sizeSpec = 0, sizeInit = 0, sizeBuf = 0;   
+	ippsDFTGetSize_C_32fc(fftlen, IPP_FFT_NODIV_BY_ANY, ippAlgHintNone, &sizeSpec, &sizeInit, &sizeBuf); // this just fills the 3 integers
+	/* memory allocation */
+	IppsDFTSpec_C_32fc **pDFTSpec = (IppsDFTSpec_C_32fc**)ippMalloc(sizeof(IppsDFTSpec_C_32fc*)*NUM_THREADS);
+	Ipp8u **pDFTBuffer = (Ipp8u**)ippMalloc(sizeof(Ipp8u*)*NUM_THREADS);
+	Ipp8u **pDFTMemInit = (Ipp8u**)ippMalloc(sizeof(Ipp8u*)*NUM_THREADS);
+	for (t = 0; t<NUM_THREADS; t++){ // make one for each thread
+		pDFTSpec[t] = (IppsDFTSpec_C_32fc*)ippMalloc(sizeSpec); // this is analogue of the fftw plan
+		pDFTBuffer[t] = (Ipp8u*)ippMalloc(sizeBuf);
+		pDFTMemInit[t] = (Ipp8u*)ippMalloc(sizeInit);
+		ippsDFTInit_C_32fc(fftlen, IPP_FFT_NODIV_BY_ANY, ippAlgHintNone,  pDFTSpec[t], pDFTMemInit[t]); // kinda like making the fftw plan?
 	}
-	
-	fin = fftw_alloc_complex(fftlen*NUM_THREADS);
-	fout = fftw_alloc_complex(fftlen*NUM_THREADS);
-
-    allplans[0] = fftw_plan_dft_1d(fftlen, fin, fout, FFTW_BACKWARD, FFTW_ESTIMATE); // FFTW_MEASURE seems to cut execution time by ~10%, but FFTW_ESTIMATE takes ~0.001s whereas MEASURE takes ~0.375s
-
-    
-    for (i=1;i<NUM_THREADS;i++){
-        allplans[i] = fftw_plan_dft_1d(fftlen, &fin[fftlen*i], &fout[fftlen*i], FFTW_BACKWARD, FFTW_ESTIMATE); // make the other plans, not executing them yet
-    }
-    
-   HANDLE ThreadList[NUM_THREADS]; // handles to threads
-	    // // stuff for pthreads
-	    // pthread_t ThreadList[NUM_THREADS];
-	    // pthread_attr_t attr;
-	    // pthread_attr_init(&attr);
-	    // pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
-    
-    for (t=0; t<NUM_THREADS; t++){
+	// ================================================================
+	for (t=0; t<NUM_THREADS; t++){
 		thread_data_array[t].thread_t_ID = t;
+		thread_data_array[t].thread_NUMTHREADS = NUM_THREADS;
 		
 		thread_data_array[t].thread_f_tap = f_tap;
 		thread_data_array[t].thread_L = L;
@@ -148,43 +137,43 @@ extern DLL_EXPORT int cpuWola(fftw_complex *y, double *f_tap, int fftlen, int De
 		thread_data_array[t].thread_nprimePts = nprimePts;
 		thread_data_array[t].thread_y = y;
 		
-		thread_data_array[t].thread_fin = &fin[t*fftlen];
-		thread_data_array[t].thread_fout = &fout[t*fftlen];
-		
-		thread_data_array[t].thread_tones = tones;
+		thread_data_array[t].thread_pDFTBuffer = pDFTBuffer[t];
+		thread_data_array[t].thread_pDFTSpec = pDFTSpec[t];
 
 		thread_data_array[t].thread_out = out; // for R2018
 		
-         // pthread_create(&ThreadList[t], &attr, threaded_wola, (void *)&thread_data_array[t]);
+        // pthread_create(&ThreadList[t], &attr, threaded_wola, (void *)&thread_data_array[t]);
 		ThreadList[t] = (HANDLE)_beginthreadex(NULL,0,&threaded_wola,(void*)&thread_data_array[t],0,NULL);
 
-        // printf("Beginning threadID %i..\n",thread_data_array[t].thread_t_ID);
+        printf("Beginning threadID %i..\n",thread_data_array[t].thread_t_ID);
 	}
-
-    // for (i = 0; i < NUM_THREADS; i++) {
-	    // if(pthread_join(ThreadList[i], NULL)) { // this essentially waits for all above threads
-			    // fprintf(stderr, "Error joining threadn");
-			    // return 2;
-	    // }
-    // }
     
-   WaitForMultipleObjects(NUM_THREADS,ThreadList,1,INFINITE);
+    WaitForMultipleObjects(NUM_THREADS,ThreadList,1,INFINITE);
 
 	// ============== CLEANUP =================
-   // close threads
-   // printf("Closing threads...\n");
-   for(t=0;t<NUM_THREADS;t++){
-       CloseHandle(ThreadList[t]);
+    // close threads
+    printf("Closing threads...\n");
+    for(t=0;t<NUM_THREADS;t++){
+        CloseHandle(ThreadList[t]);
 //         printf("Closing threadID %i.. %i\n",(int)ThreadIDList[t],WaitForThread[t]);
-   }
-   printf("All threads closed! \n");
+    }
+    printf("All threads closed! \n");
 
-    for (i=0;i<NUM_THREADS;i++){fftw_destroy_plan(allplans[i]);}
+	// === FINAL CLEANUP ===
+	for (t=0; t<NUM_THREADS; t++){
+		ippFree(pDFTSpec[t]);
+		ippFree(pDFTBuffer[t]);
+		ippFree(pDFTMemInit[t]);
+	}
+	ippFree(pDFTSpec);
+	ippFree(pDFTBuffer);
+	ippFree(pDFTMemInit);
 
-    fftw_free(fin);
-    fftw_free(fout);
-	
-	ippsFree(tones);
-
-	return 0;
+	free(ThreadList);
+	free(thread_data_array);
 }
+
+
+#ifdef __cplusplus
+}
+#endif

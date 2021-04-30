aa0389bbbc521a1b498f07bc1bc1415b73f315e0
icyveins7 <icyveins@hotmail.com>
2021-04-30 17:07:51 +0800

    add custom GPU kernel for toneSpectrum (single and batch)
    single is about 10-12x faster than CPU. but batch is almost 70 times
    faster than that! i.e. batch speed almost 1000x faster!

--- spectralRoutines.py
+++ spectralRoutines.py
@@ -8,6 +8,8 @@ Created on Wed Apr  7 16:26:26 2021
 import numpy as np
 import sympy
 from numba import jit
+import cupy as cp
+import time
 
 #%%
 def czt(x, f1, f2, binWidth, fs):
@@ -83,3 +85,220 @@ def toneSpectrum(f0, freqs, fs, N, phi=0, A=1.0):
     vals = -1j * A * (1 - np.exp(-1j*2*np.pi*(freqs-f0)*N/fs))/(2*np.pi*(freqs-f0)/fs) * np.exp(1j*phi)
     
     return vals
+
+toneSpecKernel = cp.RawKernel(r'''
+#include <cupy/complex.cuh>
+#include <math_constants.h>
+extern "C" __global__
+void toneSpec_kernel(const double phi, const double f0, const double *freqs, int freqslen,
+                     const double fs, const int N, const double A,
+                     complex<double> *out)
+{
+    int tid = blockDim.x * blockIdx.x + threadIdx.x;
+
+    const double pi = 3.141592653589793;
+    double phase;
+    double common;
+    double sinpart, cospart;
+    complex<double> coeff;
+    complex<double> phaseTerm;
+    complex<double> numerator;
+    
+    // calculate the external phase first
+    phase = phi - pi/2.0;
+    sincos(phase, &sinpart, &cospart);
+    phaseTerm = complex<double>(cospart, sinpart);
+
+    for (int i = tid; i < freqslen; i += gridDim.x * blockDim.x)
+    {
+        common = 2.0 * (freqs[i] - f0) / fs;
+        coeff = A/(common * pi);
+        
+
+        // calculate the numerator
+        phase = -common * N;
+        sincospi(phase, &sinpart, &cospart);
+        numerator = 1.0 - complex<double>(cospart, sinpart);
+        
+        // final output
+        out[i] = coeff * phaseTerm * numerator;
+
+    }
+}
+''', 'toneSpec_kernel')  
+
+
+def toneSpectrum_gpu(f0, d_freqs, fs, N, phi=0.0, A=1.0):
+    '''
+    Note that using this kernel is way faster than expressing it plainly in pythonic cupy (est ~7-8 times slower).
+    '''
+    out = cp.zeros((d_freqs.size), dtype=cp.complex128)
+    
+    THREADS_PER_BLOCK = 256
+    NUM_BLOCKS = int(np.ceil(out.size/THREADS_PER_BLOCK) + 1)
+    toneSpecKernel((NUM_BLOCKS,),(THREADS_PER_BLOCK,), (phi, f0, d_freqs, d_freqs.size, np.float64(fs), int(N), A, out))
+                
+    return out
+
+toneSpecMultiKernel = cp.RawKernel(r'''
+#include <cupy/complex.cuh>
+#include <math_constants.h>
+extern "C" __global__
+void toneSpecMulti_kernel(const double *phi, const double *f0, const double *freqs, int freqslen,
+                     const double fs, const int N, const double *A,
+                     int m, complex<double> *out)
+{
+    int tid = blockDim.x * blockIdx.x + threadIdx.x;
+    int blocksPerRow = freqslen/blockDim.x + ((freqslen % blockDim.x) > 0); // add 1 so that block exceeds the row length
+    int blockLoadIdx = blockIdx.x % blocksPerRow;
+
+    int row = tid/(blocksPerRow*blockDim.x);
+    int col = tid%(blocksPerRow*blockDim.x);
+    
+    // Each block will process m rows * blockDim cols
+    // That means each block need only read (blockDim) global memory values of freqs,
+    // (m) values of phi, f0 and A.
+    // It should be enforced that m < blockDim.
+    
+    // Load these into shared memory
+    extern __shared__ double s[];
+    double *s_phi = s; // (m) doubles
+    double *s_f0 = (double*)&s_phi[m]; // (m) doubles
+    double *s_A = (double*)&s_f0[m]; // (m) doubles
+    double *s_freqs = (double*)&s_A[m]; // (blockDim.x) doubles
+    
+    // load shared memory
+    for (int t = threadIdx.x; t < m; t = t + blockDim.x){
+        s_phi[t] = phi[t];
+    }
+    for (int t = threadIdx.x; t < m; t = t + blockDim.x){
+        s_f0[t] = f0[t];
+    }
+    for (int t = threadIdx.x; t < m; t = t + blockDim.x){
+        s_A[t] = A[t];
+    }
+    for (int t = threadIdx.x; t < min(blockDim.x, freqslen); t = t + blockDim.x){ // either load up to the blockDim, or if freqs is shorter then to the freqslen
+        s_freqs[t] = freqs[blockLoadIdx * blockDim.x + t];
+    }
+    
+    
+    __syncthreads();
+
+    const double pi = 3.141592653589793;
+    double phase;
+    double common;
+    double sinpart, cospart;
+    complex<double> coeff;
+    complex<double> phaseTerm;
+    complex<double> numerator;
+    
+    double u_phi, u_f0, u_A;
+    
+    for (int mi = 0; mi < m; mi++){
+        // broadcast used constants from shared mem for this iteration
+        u_phi = s_phi[mi];
+        u_f0 = s_f0[mi];
+        u_A = s_A[mi];
+            
+        for (int i = threadIdx.x; i < blockDim.x; i += gridDim.x * blockDim.x)
+        {
+            common = 2.0 * (s_freqs[i] - u_f0) / fs; // each thread will read one value from shared mem
+            coeff = u_A/(common * pi);
+            
+            // calculate the external phase first
+            phase = u_phi - pi/2.0;
+            sincos(phase, &sinpart, &cospart);
+            phaseTerm = complex<double>(cospart, sinpart);
+    
+            // calculate the numerator
+            phase = -common * N;
+            sincospi(phase, &sinpart, &cospart);
+            numerator = 1.0 - complex<double>(cospart, sinpart);
+            
+            // final output to global mem directly if it doesn't overshoot
+            if (col < freqslen){
+                out[mi * freqslen + col] = coeff * phaseTerm * numerator;
+            }
+        }
+    }
+}
+''', 'toneSpecMulti_kernel')  
+
+
+def toneSpectrumMulti_gpu(d_f0List, d_freqs, fs, N, d_phiList, d_AList):
+    out = cp.zeros((d_f0List.size, d_freqs.size), dtype=cp.complex128)
+    
+    THREADS_PER_BLOCK = 256
+    NUM_BLOCKS = int(out.size/THREADS_PER_BLOCK) + int((out.size%THREADS_PER_BLOCK)>0)
+    
+    if d_f0List.size > THREADS_PER_BLOCK:
+        raise Exception('Reduce parameter size. %d > %d' % (d_f0List.size > THREADS_PER_BLOCK))
+    
+    toneSpecMultiKernel((NUM_BLOCKS,),(THREADS_PER_BLOCK,), (d_phiList, d_f0List, d_freqs, d_freqs.size, np.float64(fs), int(N), d_AList, d_f0List.size, out),
+                        shared_mem=(THREADS_PER_BLOCK+3*d_f0List.size)*8)
+                
+    return out
+
+
+if __name__=='__main__':
+    # Unit testing for GPU code
+    fs = 192000
+    
+    fineFreqRange = 5.0
+    fineFreqStep = 0.001
+    freqs = np.arange(-fineFreqRange,fineFreqRange+fineFreqStep/2, fineFreqStep)
+    N = 145152
+    
+    phiSearch = np.arange(0,2*np.pi, 0.001)[:5000]
+    
+    f0 = 0.312
+    
+    t1 = time.time()
+    
+    for i in range(len(phiSearch)):
+        ts = toneSpectrum(f0, freqs, fs, N, phi=phiSearch[i])
+        
+    t2 = time.time()
+    print("CPU: %f s." % (t2-t1))
+    
+    
+    
+    d_freqs = cp.array(freqs)
+    t1 = time.time()
+    
+    for i in range(len(phiSearch)):
+        tsg = toneSpectrum_gpu(f0, d_freqs, fs, N, phi=phiSearch[i])
+        
+    t2 = time.time()
+    print("Naive GPU Kernel: %f s." % (t2-t1))
+    print("Check:")
+    mi = np.argmax(np.abs(cp.asnumpy(tsg)-ts))
+    print(tsg[mi])
+    print(ts[mi])
+    
+    
+    #%%
+    d_f0List = cp.zeros(50) + f0
+    d_AList = cp.zeros(50) + 1
+    d_phiSearch = cp.array(phiSearch)
+    
+    t1 = time.time()
+    
+    
+    for i in range(int(len(phiSearch)/50)):
+        d_phiList = d_phiSearch[i*50:(i+1)*50]
+        tsgm = toneSpectrumMulti_gpu(d_f0List, d_freqs, fs, N, d_phiList, d_AList)
+        
+    
+    t2 = time.time()
+    print("Batch-wise GPU Kernel: %f s." % (t2-t1))
+    
+    tsgcheck = cp.zeros((50,d_freqs.size), cp.complex128)
+    for k in range(50):
+        tsgcheck[k] = toneSpectrum_gpu(float(d_f0List[k]), d_freqs, fs, N, float(d_phiList[k]), float(d_AList[k]))
+    
+    print("Check:")
+    mi = cp.argmax(cp.abs(tsgcheck.flatten()-tsgm.flatten()))
+    print(tsgcheck.flatten()[mi])
+    print(tsgm.flatten()[mi])
+

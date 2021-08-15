ca402726f94b6ba5de0a03d899a7cd64bc7df3ae
unknown <icyveins@hotmail.com>
2021-08-16 05:19:15 +0800

    more edits to musicRoutines run tests

--- musicRoutines.py
+++ musicRoutines.py
@@ -42,14 +42,24 @@ def musicAlg(x, freqlist, rows, p):
 
 
 if __name__ == '__main__':
+    plt.close("all")
     fs = 1e4
     length = 1*fs
     fdiff = 0.5
     f0 = 1000
     # x = np.exp(1j*2*np.pi*f0*np.arange(length)/fs) + np.exp(1j*2*np.pi*(f0+fdiff)*np.arange(length)/fs)
     x = np.pad(np.exp(1j*2*np.pi*f0*np.arange(length)/fs), (100,0))
-    x = x + np.pad(np.exp(1j*2*np.pi*(f0+fdiff)*np.arange(length)/fs), (0,100))
-    # x = x + (np.random.randn(x.size) + np.random.randn(x.size)*1j) * 0.5
+    
+    # noisyAmp = (np.random.randn(int(length+100))*0.000001+1.0)
+    # noisyAmp = np.abs(noisyAmp)
+    noisyAmp = 1.0
+    
+    # How does it react to a non-constant amplitude tone?
+    plt.figure("Amplitude for second tone")
+    plt.plot(noisyAmp)
+    
+    x = x + noisyAmp * np.pad(np.exp(1j*2*np.pi*(f0+fdiff)*np.arange(length)/fs), (100,0)) * np.exp(1j*np.pi*2/4)
+    x = x + (np.random.randn(x.size) + np.random.randn(x.size)*1j) * 1e-3
     # xfft = np.fft.fft(x)    
     
     fineFreqStep = 0.01
@@ -58,12 +68,26 @@ if __name__ == '__main__':
     xczt = czt(x, (f0+fdiff/2)-fineFreqRange,(f0+fdiff/2)+fineFreqRange, fineFreqStep, fs)
     
     freqlist = np.arange(999,1003,0.1)
-    f,u,s,vh = musicAlg(x, freqlist/fs, 5, 2) # normalize freq
-    
-    plt.figure()
+    plt.figure("Comparison")
     # plt.plot(makeFreq(len(x),fs), np.abs(xfft)/np.max(np.abs(xfft)))
     plt.plot(fineFreqVec, np.abs(xczt)/ np.max(np.abs(xczt)), label='CZT')
-    plt.plot(freqlist, f/np.max(f), label='MUSIC')
     plt.vlines([f0,f0+fdiff],0,1,colors='r', linestyles='dashed',label='Actual')
-    plt.legend()
-    plt.xlim([990,1010])
+    for i in range(1,5):
+        f,u,s,vh = musicAlg(x, freqlist/fs, 1000, i) # normalize freq
+        
+        plt.figure("Comparison")
+        plt.plot(freqlist, f/np.max(f), label='MUSIC, p='+str(i))
+        plt.legend()
+        plt.xlim([fineFreqVec[0],fineFreqVec[-1]])
+        
+    plt.figure("Eigenvalues")
+    plt.plot(s,'x-')
+    
+    ## At 1e-4 noise:
+    # Note that at rows=100, unable to resolve. But at rows = 1000, able to resolve (dependency on 'block size' despite total length being equal)
+    # p=3 is now required, instead of p=2
+    ## At 1e-3 noise:
+    # Similarly, at rows = 1000, able to resolve, but clarity is diminished.
+    # Still p=3
+    ## At 1e-2 noise:
+    # No longer able to resolve with rows=1000.

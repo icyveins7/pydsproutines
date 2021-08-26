3d3715610edb34392aa7d18d96dad54ae43ca15e
unknown <icyveins@hotmail.com>
2021-08-26 15:52:01 +0800

    add class rework of music with subclass
    to get ready for esprit, which also uses covariance matrix. also edited
    the test code to have generic multiple tones (realised that resolution
    fails when adding more tones again..)

--- musicRoutines.py
+++ musicRoutines.py
@@ -120,9 +120,6 @@ def musicAlg(x, freqlist, rows, plist, snapshotJump=None, fwdBwd=False,
     
     u, s, vh = np.linalg.svd(Rx)
     
-    f = np.zeros(len(freqlist))
-    
-   
     # Instead of iterations, generate a one-time Vandermonde matrix of eh
     ehlist = np.exp(-1j*2*np.pi*freqlist.reshape((-1,1))*np.arange(rows)) # generate the e vector for every frequency i.e. Vandermonde
     
@@ -157,42 +154,223 @@ def musicAlg(x, freqlist, rows, plist, snapshotJump=None, fwdBwd=False,
         
     return f, u, s, vh
 
+#############################
+class CovarianceTechnique:
+    def __init__(self, rows, snapshotJump=None, fwdBwd=False, avgToToeplitz=False):
+        '''
+        snapshotJump is the index jump per column vector. By default this jump is equal to rows,
+        i.e. each column vector is unique (matrix constructed via reshape), but may not resolve frequencies well.
+        
+        fwdBwd is a boolean which toggles the use of the Forward-Backward correction of the covariance matrix (default False).
+        
+        averageToToeplitz is a boolean which toggles averaging of the covariance matrix along each diagonal.
+        This ensures a full rank matrix. Defaults to False.
+        '''
+        if snapshotJump is not None and snapshotJump <= 0:
+            raise ValueError("snapshotJump must be at least 1.")
+        
+        self.rows = rows
+        self.snapshotJump = snapshotJump
+        self.fwdBwd = fwdBwd
+        self.avgToToeplitz = avgToToeplitz
+        
+    def calcRx(self, x):
+        if isinstance(x, dict): # If input is multiple 1-d arrays
+            # Instantiate the final mat (empty at first)
+            xs = np.zeros((rows,0), np.complex128)
+            # Iterate over every 1-d array in the dictionary
+            for xi in x.values():
+                if self.snapshotJump is None: # Refer to the single-array version for comments
+                    xi = xi.reshape((-1,1)) # vectorize
+                    cols = int(np.floor(len(xi)/rows))
+                    xslen = rows * cols
+                    xs_i = xi[:xslen] # we cut off the ending bits
+                    xs_i = xs_i.reshape((cols, rows)).T
+                else: # Refer to the single-array version for comments 
+    
+                    xi = xi.flatten() # in case it's not flat
+                    cols = (xi.size - rows) / self.snapshotJump # calculate the required columns
+                    xs_i = np.zeros((rows, int(cols+1)), xi.dtype)
+                    print("Matrix dim is %d, %d" % (xs.shape[0], xs.shape[1]))
+                    for i in range(xs_i.shape[1]): # fill the columns
+                        xs_i[:,i] = xi[i * self.snapshotJump : i * self.snapshotJump + rows]
+                
+                # Then, concatenate the matrix
+                xs = np.hstack((xs, xs_i))
+            
+            
+        else: # If input is just a single 1-d array
+            if self.snapshotJump is None: # then we snapshot via slices
+                # 1 2 3 4 5 6 ->
+                # 1 3 5
+                # 2 4 6  for example
+                x = x.reshape((-1,1)) # vectorize
+                cols = int(np.floor(len(x)/rows))
+                xslen = rows * cols
+                xs = x[:xslen] # we cut off the ending bits
+                xs = xs.reshape((cols, rows)).T
+            else: # we use the rate to populate our columns
+                # e.g. for snapshot jump = 1,
+                # 1 2 3 4 5 6 ->
+                # 1 2 3 4 5
+                # 2 3 4 5 6
+                    
+                x = x.flatten() # in case it's not flat
+                cols = (x.size - rows) / self.snapshotJump # calculate the required columns
+                xs = np.zeros((rows, int(cols+1)), x.dtype)
+                print("Matrix dim is %d, %d" % (xs.shape[0], xs.shape[1]))
+                for i in range(xs.shape[1]): # fill the columns
+                    xs[:,i] = x[i * self.snapshotJump : i * self.snapshotJump + rows]
+                
+        Rx = (1/cols) * xs @ xs.conj().T
+        if self.fwdBwd:
+            J = np.eye(Rx.shape[0])
+            # Reverse row-wise to form the antidiagonal exchange matrix
+            J = J[:,::-1]
+            Rx = 0.5 * (Rx + J @ Rx.T @ J)
+            print("Using forward-backward covariance.")
+        
+        if self.avgToToeplitz:
+            diagIdx = np.arange(-Rx.shape[0]+1, Rx.shape[1])
+            
+            Rx_tp = np.zeros_like(Rx)
+            for k in diagIdx:
+                diagArr = np.zeros(Rx.shape[0]-np.abs(k), Rx.dtype) + np.mean(np.diag(Rx,k))
+                diagMat = np.diag(diagArr, k) # make it into a matrix again
+                Rx_tp = Rx_tp + diagMat
+            
+    
+        u, s, vh = np.linalg.svd(Rx)
+        
+        return u, s, vh
+    
+class MUSIC(CovarianceTechnique):
+    def __init__(self, rows, snapshotJump=None, fwdBwd=False, avgToToeplitz=False):
+        super().__init__(rows, snapshotJump, fwdBwd, avgToToeplitz)
+        
+    def run(self, x, freqlist, plist, useSignalAsNumerator=False):
+        '''
+        Parameters
+        ----------
+        x : 1-dim array or dictionary of 1-dim arrays.
+            Input array/dictionary. If x is passed in as a dictionary,
+            each value in the dictionary is expected to be a 1-dim array; each 1-dim array is parsed into its own
+            matrix using 'snapshotJump', and then the matrices are stacked horizontally to form a large data matrix.
+            This is then used to estimate the covariance matrix. 
+            Note: the 1-dim arrays do not need to be the same length,
+            since they are processed using the snapshotJump parameter.
+        freqlist : 1-dim array or list
+            List of freq values to calculate pseudospectrum at.
+        plist : scalar/1-dim array or list
+            p (i.e. plist) is the dimensionality of the signal subspace. The function will return an array based on freqlist
+            for every value of p in plist.
+        useSignalAsNumerator : bool, optional
+            Toggles the use of the signal subspace as the numerator. The default is False.
+
+        Returns
+        -------
+        f : 1-dim array or 2-d matrix.
+            Pseudospectrum values. If plist has multiple values, each row corresponds to the pseudospectrum based on that p value.
+        u : Matrix
+            Output from SVD.
+        s : Matrix
+            Output from SVD.
+        vh : Matrix
+            Output from SVD.
+
+        '''
+        u, s, vh = self.calcRx(x)
+        
+        # Instead of iterations, generate a one-time Vandermonde matrix of eh
+        ehlist = np.exp(-1j*2*np.pi*freqlist.reshape((-1,1))*np.arange(rows)) # generate the e vector for every frequency i.e. Vandermonde
+        
+        # Generate output
+        numerator = 1.0 # default numerator
+        if not hasattr(plist, '__len__'): # if only one value of p
+            d = ehlist @ u[:,plist:]
+            denom = np.sum(np.abs(d)**2, axis=1)
+            
+            if useSignalAsNumerator: # Construct the generalised inverse (we have the SVD results already)
+                ssp = s[:plist]**-0.5 # Take the p eigenvalues and reciprocal + root them -> generalised inverse root eigenvalues
+                siginv = u[:,:plist] * ssp # assume u = v, i.e. u^H = v^H, scale by the inverse eigenvalues
+                n = ehlist @ siginv
+                numerator = np.sum(np.abs(n)**2, axis=1)
+                
+            f = numerator / denom
+            
+        else: # for multiple values of p
+            f = np.zeros((len(plist), len(freqlist)))
+            for i in range(len(plist)):
+                p = plist[i]
+                d = ehlist @ u[:,p:]
+                denom = np.sum(np.abs(d)**2, axis=1)
+                
+                if useSignalAsNumerator:
+                    ssp = s[:p]**-0.5 # Take the p eigenvalues and reciprocal + root them -> generalised inverse root eigenvalues
+                    siginv = u[:,:p] * ssp # assume u = v, i.e. u^H = v^H, scale by the inverse eigenvalues
+                    n = ehlist @ siginv
+                    numerator = np.sum(np.abs(n)**2, axis=1)
+                    
+                f[i,:] = numerator / denom
+                
+        # Return
+        return f,u,s,vh
+        
+        
+
 #%%
 if __name__ == '__main__':
     #%%
     plt.close("all")
     fs = 1e4
-    length = 1*fs
-    fdiff = 0.5
-    f0 = 1000
+    length = 1.0*fs
+    fdiff = 1.5
+    f0 = 999
     padding = 0
-    # x = np.exp(1j*2*np.pi*f0*np.arange(length)/fs) + np.exp(1j*2*np.pi*(f0+fdiff)*np.arange(length)/fs)
-    x = np.pad(np.exp(1j*2*np.pi*f0*np.arange(length)/fs), (padding,0))
-    
+    f_true = []
+    numTones = 3
+    # Add tones
+    x = np.zeros(int(length),dtype=np.complex128)
+    for i in range(numTones):
+        x = x + np.pad(np.exp(1j*2*np.pi*(f0+i*fdiff)*np.arange(length)/fs), (padding,0))
+        f_true.append(f0+i*fdiff)
+    # Add some noise
     noisePwr = 1e-1
-    x = x + np.pad(np.exp(1j*2*np.pi*(f0+fdiff)*np.arange(length)/fs), (0,padding))
     x = x + (np.random.randn(x.size) + np.random.randn(x.size)*1j) * np.sqrt(noisePwr)
-    # xfft = np.fft.fft(x)    
-    
+
+    # Calculate CZT
     fineFreqStep = 0.01
     fineFreqRange = 3 # peg to the freqoffset
     fineFreqVec = np.arange((f0+fdiff/2)-fineFreqRange,(f0+fdiff/2)+fineFreqRange + 0.1*fineFreqStep, fineFreqStep)
     xczt = czt(x, (f0+fdiff/2)-fineFreqRange,(f0+fdiff/2)+fineFreqRange, fineFreqStep, fs)
     
-    freqlist = np.arange(f0-fdiff,f0+fdiff*2,0.01)
+    freqlist = np.arange(np.min(f_true)-fdiff,np.max(f_true)+fdiff,0.01)
     
     # One-shot evaluation for all desired p values
-    plist = np.arange(1,5)
+    plist = np.arange(3,5)
     rows = 1000
     t1 = time.time()
     f, u, s, vh = musicAlg(x, freqlist/fs, rows, plist, snapshotJump=1)
     # f_tp, u_tp, s_tp, vh_tp = musicAlg(x, freqlist/fs, rows, plist, snapshotJump=1, averageToToeplitz=True) # Takes very long
     f_fb, u_fb, s_fb, vh_fb = musicAlg(x, freqlist/fs, rows, plist, snapshotJump=1, fwdBwd=True)
     f_fb_ns, u_fb_ns, s_fb_ns, vh_fb_ns = musicAlg(x, freqlist/fs, rows, plist, snapshotJump=1, fwdBwd=True, useSignalAsNumerator=True)
-    f_ac, u_ac, s_ac, vh_ac = musicAlg(x, freqlist/fs, rows, plist, useAutoCorr=True)
+    # f_ac, u_ac, s_ac, vh_ac = musicAlg(x, freqlist/fs, rows, plist, useAutoCorr=True)
     t2 = time.time()
     print("Took %f s." % (t2-t1))
     
+    ## OOP Rework
+    music = MUSIC(rows, snapshotJump=1)
+    # Standard
+    ff, uu, ss, vvh = music.run(x, freqlist/fs, plist)
+    assert(np.all(f==ff))
+    # Forward-Backward
+    music.fwdBwd = True
+    ff_fb, uu_fb, ss_fb, vvh_fb = music.run(x, freqlist/fs, plist)
+    assert(np.all(f_fb==ff_fb))
+    # Forward-Backward with Signal Subspace
+    ff_fb_ns, uu_fb_ns, ss_fb_ns, vvh_fb_ns = music.run(x, freqlist/fs, plist, useSignalAsNumerator=True)
+    assert(np.all(ff_fb_ns==f_fb_ns))
+
     fig,ax = plt.subplots(4,1,num="Comparison")
     ax[0].set_title("Standard MUSIC, %d rows" % (rows))
     # ax[1].set_title("Averaged Toeplitz MUSIC, %d rows" % (rows))
@@ -203,7 +381,7 @@ if __name__ == '__main__':
     for i in range(len(ax)):
         # plt.plot(makeFreq(len(x),fs), np.abs(xfft)/np.max(np.abs(xfft)))
         ax[i].plot(fineFreqVec, np.abs(xczt)/ np.max(np.abs(xczt)), label='CZT')
-        ax[i].vlines([f0,f0+fdiff],0,1,colors='r', linestyles='dashed',label='Actual')
+        ax[i].vlines(f_true,0,1,colors='r', linestyles='dashed',label='Actual')
    
     for i in range(f.shape[0]):
         ax[0].plot(freqlist, f[i]/np.max(f[i]), label='MUSIC, p='+str(plist[i]))
@@ -214,9 +392,9 @@ if __name__ == '__main__':
         # ax[1].legend()
         # ax[1].set_xlim([fineFreqVec[0],fineFreqVec[-1]])
         
-        ax[1].plot(freqlist, f_ac[i]/np.max(f_ac[i]), label='MUSIC, p='+str(plist[i]))
-        ax[1].legend()
-        ax[1].set_xlim([fineFreqVec[0],fineFreqVec[-1]])
+        # ax[1].plot(freqlist, f_ac[i]/np.max(f_ac[i]), label='MUSIC, p='+str(plist[i]))
+        # ax[1].legend()
+        # ax[1].set_xlim([fineFreqVec[0],fineFreqVec[-1]])
         
         ax[2].plot(freqlist, f_fb[i]/np.max(f_fb[i]), label='MUSIC, p='+str(plist[i]))
         ax[2].legend()
@@ -230,7 +408,7 @@ if __name__ == '__main__':
     plt.figure("Eigenvalues")
     plt.plot(np.log10(s),'x-',label='Standard MUSIC')
     # plt.plot(np.log10(s_tp),'x-',label='Averaged Toeplitz MUSIC')
-    plt.plot(np.log10(s_ac),'x-',label='Auto-correlation MUSIC')
+    # plt.plot(np.log10(s_ac),'x-',label='Auto-correlation MUSIC')
     plt.plot(np.log10(s_fb),'x-',label='Forward-Backward MUSIC')
     plt.plot(np.log10(s_fb_ns),'x-',label='Forward-Backward + Signal Subspace MUSIC')
     plt.legend()
@@ -253,6 +431,7 @@ if __name__ == '__main__':
     ## For best results, rows = 1000 at noisePwr 1e-1 (ie about 10dB) with both forward-backward
     ## and signal subspace enabled appears to be the best performing.
     
+    assert(False)
     #%% Experiment with separated bursts
     numBursts = 10
     burstLength = int(fs * 0.5)

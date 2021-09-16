5da23d4034427712cf79cde926aa480842ad5c61
SeoLubuntu <icyveins@hotmail.com>
2021-09-16 13:10:33 +0800

    add dashTest

---
+++ dashTest.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 16 12:41:04 2021
+
+@author: seolubuntu
+"""
+
+import dash
+from dash import dcc
+from dash import html
+import plotly.express as px
+from plotly.offline import plot
+import pandas as pd
+import numpy as np
+
+app = dash.Dash(__name__)
+
+colors = {
+    'background': '#111111',
+    'text': '#7FDBFF'
+}
+
+df = pd.DataFrame({
+    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+    "Amount": [4, 1, 2, 2, 4, 5],
+    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
+})
+
+fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+
+longdata = np.random.randn(1000000)
+longfig = px.line(x=np.arange(longdata.size),y=longdata)
+
+fig.update_layout(
+    plot_bgcolor=colors['background'],
+    paper_bgcolor=colors['background'],
+    font_color=colors['text']
+)
+
+app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[
+    html.H1(
+        children='Hello Dash',
+        style={
+            'textAlign': 'center',
+            'color': colors['text']
+        }
+    ),
+
+    html.Div(children='Dash: A web application framework for Python.', style={
+        'textAlign': 'center',
+        'color': colors['text']
+    }),
+
+    dcc.Graph(
+        id='example-graph-2',
+        figure=fig,
+        style={'display':'inline-block',
+               'width':500,
+               'margin':0}
+    ),
+    
+    dcc.Graph(
+        id='example-graph-3',
+        figure=longfig,
+        style={'display':'inline-block',
+               'width':800,
+               'margin':0}
+    )
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)

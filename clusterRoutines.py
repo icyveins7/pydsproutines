72c25cd7ad630f6a0d9bda11f9a54d15d63e9b0c
lken <lken@dso.org.sg>
2022-03-16 18:29:32 +0800

    edit clusterEngine _cluster1d to _cluster, add 2d classes
    the original _cluster method worked for 2d inputs anyway;
    2D class now has a separate plotter, and a convenience method to convert
    complex valued arrays into the appropriate 2d matrix
    angular version of the 2D is currently incomplete

--- clusterRoutines.py
+++ clusterRoutines.py
@@ -23,7 +23,7 @@ class ClusterEngine:
                             'ch': "Calinski Harabasz",
                             'db': "Davies Bouldin"}
         
-    def _cluster1d(self, x):
+    def _cluster(self, x):
   
         self.scores = {key: np.zeros(len(self.guesses)) for key in self.scoretypes}
         
@@ -79,11 +79,12 @@ class ClusterEngine:
             x = x.reshape((-1,1)) # does not overwrite external argument array
         
         # We loop until all conditions are met
-        idxUsed = np.arange(x.size) # Initialize using the entire array
+        
+        idxUsed = np.arange(len(x)) # Initialize using the entire array
         idxRemoved = []
         while(True):
             # Cluster first
-            bestguess = self._cluster1d(x[idxUsed])
+            bestguess = self._cluster(x[idxUsed])
             # Make the model with best guess of no. of clusters
             bestmodel = KMeans(n_clusters=bestguess).fit(x[idxUsed])
             # Count the cluster sizes
@@ -189,6 +190,83 @@ class ClusterEngine:
         return fig, ax
                 
                 
+#############
+class ClusterEngine2D(ClusterEngine):
+    def __init__(self, guesses, minClusterSize=None, minClusterFraction=None, scoretypes=["sil"]):
+        super().__init__(guesses, minClusterSize, minClusterFraction, scoretypes)
+        
+    @staticmethod
+    def convertComplexToInterleaved2D(x):
+        if x.dtype == np.complex64:
+            return x.view(np.float32).reshape((-1,2))
+        elif x.dtype == np.complex128:
+            return x.view(np.float64).reshape((-1,2))
+        else:
+            return x
+        
+    def cluster(self, x: np.ndarray,
+                verbose: bool=False):
+        
+        # Check if input is complex, view as interleaved
+        x = self.convertComplexToInterleaved2D(x)
+            
+        # Call the original cluster, but sorting is ignored for 2D
+        bestguess, bestmodel, idxRemoved, idxUsed = super().cluster(x, False, verbose)
+        
+        return bestguess, bestmodel, idxRemoved, idxUsed
+    
+    @staticmethod
+    def plotClusters(x, bestmodel, idxRemoved, idxUsed, colours=['r','b'],
+                     ax=None, title="Clusters"):
+
+        # Check if input is complex, view as interleaved
+        x = ClusterEngine2D.convertComplexToInterleaved2D(x)
+
+        if ax is None: # Make a new plot with the title
+            fig, ax = plt.subplots(1,1,num=title)
+        else: # Else just plot on existing axes object
+            fig = None
+            
+        # Plot all the points
+        ax.plot(x[:,0], x[:,1], 'x')
+        
+        # Plot the outliers removed
+        if idxRemoved.size > 0:
+            ax.plot(x[idxRemoved,0], x[idxRemoved,1], 'ko', label='Outliers (%d)' % idxRemoved.size)
+        
+        # Plot the clusters via labels
+        for i in range(bestmodel.n_clusters):
+            si = idxUsed[np.argwhere(bestmodel.labels_ == i)]
+            ax.plot(x[si,0], x[si,1], colours[i%len(colours)]+'s', markerfacecolor='none', label='Cluster %d (%d)' % (i, si.size))
+            
+        ax.legend()
+        
+        return fig, ax
+                
+#############
+class AngularClusterEngine(ClusterEngine2D):
+    def __init__(self, guesses, minClusterSize=None, minClusterFraction=None, scoretypes=["sil"], minAngleSep=None):
+        super().__init__(guesses, minClusterSize, minClusterFraction, scoretypes)
+        self.minAngleSep = minAngleSep
+        
+    def cluster(self, x: np.ndarray,
+                verbose: bool=False):
+        
+        # Same as 2D
+        bestguess, bestmodel, idxRemoved, idxUsed = super().cluster(x,verbose)
+        
+        # Check extra conditionals
+        if self.minAngleSep is not None:
+            # TODO: find the angle differences directly, as this results in -pi to pi being far apart again
+            if bestmodel.cluster_centers_.dtype == np.float64:
+                angleCenters = np.angle(bestmodel.cluster_centers_.view(np.complex128)).flatten()
+            elif bestmodel.cluster_centers_.dtype == np.float32:
+                angleCenters = np.angle(bestmodel.cluster_centers_.view(np.complex64)).flatten()
+                
+            # TODO: find the angle difference and compare
+            
+        
+        return bestguess, bestmodel, idxRemoved, idxUsed
         
 
     

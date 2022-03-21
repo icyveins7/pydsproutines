df75bf44491d5d1002afe5034e3c430916545689
lken <lken@dso.org.sg>
2022-03-21 18:13:47 +0800

    add setup.py for editable pip installs

---
+++ setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+setup(
+    name="pydsproutines",
+    version="1.0",
+    py_modules=[
+        'averagingEllipsesRoutines',
+        'burstyRoutines',
+        'cancellationRoutines',
+        'clusterRoutines',
+        'cyclostationaryRoutines',
+        'demodulationRoutines',
+        'filterCreationRoutines',
+        'filterRoutines',
+        'localizationRoutines',
+        'musicRoutines',
+        'plotRoutines',
+        'selectionRoutines',
+        'signalCreationRoutines',
+        'spectralRoutines',
+        'trajectoryRoutines',
+        'usrpRoutines',
+        'verifyRoutines',
+        'xcorrRoutines'],
+    )

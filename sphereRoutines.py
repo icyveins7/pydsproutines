58b16f9dd1df5e21022f61b0806bf49883656f9b
unknown <icyveins@hotmail.com>
2022-12-14 18:30:05 +0800

    refactor spheroids and plotters into ellipsoid, fix intersect
    arctan2 instead of arctan

--- sphereRoutines.py
+++ sphereRoutines.py
@@ -44,28 +44,11 @@ class Ellipsoid:
         self.Rx = Rx
         self.Rz = Rz
         
-class OblateSpheroid(Ellipsoid):
-    def __init__(self, omega: float, lmbda: float,
-                 mu: np.ndarray=np.zeros(3),
-                 Rx: np.ndarray=np.eye(3),
-                 Rz: np.ndarray=np.eye(3)):
-        
-        self.omega = omega
-        self.lmbda = lmbda
-        assert(lmbda > omega)
-        super().__init__(omega, omega, lmbda, mu, Rx, Rz)
-
-
-class Sphere(Ellipsoid):
-    def __init__(self, r: float, mu: np.ndarray=np.zeros(3)):
-        self.r = r
-        super().__init__(r, r, r, mu)
-    
     def pointsFromAngles(self, theta, phi):
         points = np.array([
-            self.r * np.sin(theta) * np.cos(phi),
-            self.r * np.sin(theta) * np.sin(phi),
-            self.r * np.cos(theta)
+            self.a * np.sin(theta) * np.cos(phi),
+            self.b * np.sin(theta) * np.sin(phi),
+            self.c * np.cos(theta)
         ])    
         return points
     
@@ -75,6 +58,53 @@ class Sphere(Ellipsoid):
         else:
             return points + self.mu.reshape((-1,1))
         
+    # Other methods    
+    def visualize(self, theta=np.arange(0,np.pi,0.001), phi=np.arange(-np.pi, np.pi, 0.1), ax=None, colour='k'):
+        if ax is None:
+            fig = plt.figure()
+            ax = fig.add_subplot(projection='3d')
+            ax.set_xlabel("x")
+            ax.set_ylabel("y")
+            ax.set_zlabel("z")
+        else:
+            fig = None
+
+        # Generate some points in a mesh
+        theta, phi = np.meshgrid(theta, phi)
+        
+        # Calculate the cartesian coordinates from parametrisation
+        points = self.pointsFromAngles(theta, phi)
+        # breakpoint()
+        points = self.transform(points)
+        
+        # Ensure equal ratios
+        ax.set_box_aspect((
+            np.ptp(points[0].reshape(-1)),
+            np.ptp(points[1].reshape(-1)),
+            np.ptp(points[2].reshape(-1))
+        )) 
+        
+        ax.plot_wireframe(points[0], points[1], points[2], color=colour)
+        
+        return ax, fig
+        
+class OblateSpheroid(Ellipsoid):
+    def __init__(self, omega: float, lmbda: float,
+                 mu: np.ndarray=np.zeros(3),
+                 Rx: np.ndarray=np.eye(3),
+                 Rz: np.ndarray=np.eye(3)):
+        
+        self.omega = omega
+        self.lmbda = lmbda
+        assert(lmbda < omega)
+        super().__init__(omega, omega, lmbda, mu, Rx, Rz)
+
+
+class Sphere(Ellipsoid):
+    def __init__(self, r: float, mu: np.ndarray=np.zeros(3)):
+        self.r = r
+        super().__init__(r, r, r, mu)
+ 
     def intersectOblateSpheroid(self, theta, omega, lmbda):
         rs = self.r * np.sin(theta)
         rc = self.r * np.cos(theta)
@@ -86,7 +116,7 @@ class Sphere(Ellipsoid):
         B = lmbda**2 * 2 * rs * self.mu[1]
         
         with np.errstate(divide='ignore', invalid='ignore'):
-            alpha = np.arctan(B/A)
+            alpha = np.arctan2(B,A) # do not use arctan! make sure its arctan2!
             t = (lmbda**2 * omega**2 -beta-gamma)/np.sqrt(A**2 + B**2)
             # breakpoint()
             basic = np.arccos(t) # returns [0, pi]
@@ -109,57 +139,26 @@ class Sphere(Ellipsoid):
         
         return points
     
-    # Other methods    
-    def visualize(self, theta=np.arange(0,np.pi,0.001), ax=None, bothSheets=False, useSurf=False):
-        if ax is None:
-            fig = plt.figure()
-            ax = fig.add_subplot(projection='3d')
-        ax.set_xlabel("x")
-        ax.set_ylabel("y")
-        ax.set_zlabel("z")
-        
-        
-        # Generate some points in a mesh
-        phi = np.arange(-np.pi, np.pi, 0.1)
-        theta, phi = np.meshgrid(theta, phi)
-        
-        # Calculate the cartesian coordinates from parametrisation
-        points = self.pointsFromAngles(theta, phi)
-        # breakpoint()
-        points = self.transform(points)
-        
-        # Ensure equal ratios
-        ax.set_box_aspect((
-            np.ptp(points[0].reshape(-1)),
-            np.ptp(points[1].reshape(-1)),
-            np.ptp(points[2].reshape(-1))
-        )) 
-        
-        ax.plot_wireframe(points[0], points[1], points[2], color='k')
-        
-        return ax, fig
+    
     
     
 #%% Testing
 if __name__ == "__main__":
     closeAllFigs()
-    sphere = Sphere(1, np.array([1,2,0]))
+    sphere = Sphere(1, np.array([-1,-2,0]))
     
     theta = np.arange(0, np.pi, 0.01)
     omega = 2.0
     lmbda = 1.5
     points = sphere.intersectOblateSpheroid(theta, omega, lmbda)
     
+    # Show the sphere
     ax, fig = sphere.visualize()
-    # Create spheroid
-    theta = np.arange(0, np.pi, 0.1)
-    phi = np.arange(0, 2*np.pi, 0.1)
-    theta, phi = np.meshgrid(theta, phi)
-    x = omega * np.sin(theta) * np.cos(phi)
-    y = omega * np.sin(theta) * np.sin(phi)
-    z = lmbda * np.cos(theta)
-    ax.plot_wireframe(x,y,z,linestyle='--')
-    ax.set_box_aspect(None)
+    
+    # Show the oblatespheroid
+    oblate = OblateSpheroid(omega, lmbda)
+    oblate.visualize(ax=ax, colour='b')
+    # # Create spheroid
     ax.plot3D(points[0], points[1], points[2], 'r-')
     
     # Check if points truly lie on surface

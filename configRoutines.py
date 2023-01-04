4048836eeefa6a44797124249a428bae8b2f8edf
seoxubuntu <icyveins@hotmail.com>
2023-01-04 19:50:21 +0800

    update configRoutines with new formats, unfinished

--- configRoutines.py
+++ configRoutines.py
@@ -4,6 +4,41 @@
 Created on Sun Jan  1 22:50:36 2023
 
 @author: seoxubuntu
+
+The goal of these classes is to adhere to a strict, concise config file arrangement.
+As this is a DSP-related config, we define the configs as having sections related to distinct tasks:
+    
+    1) Sources: Loading/reading samples
+    Each source is usually associated with a particular path or directory,
+    along with typical things like sample rate, centre frequency etc.
+    
+    2) Signals
+    Each signal to be processed usually requires knowledge of properties such as
+    baud rate, centre frequency etc.
+    
+    3) Processing
+    The same signal may be processed in different ways if using different sources;
+    maybe different thresholds, or different number of filter taps if sample rates are not identical etc.
+    As such, the processing draws a link between a named signal and source.
+    
+We then combine these into distinct 'workspace' configs which aggregate 1 or more source configs,
+signal configs and processing configs. A typical config file may look something like this:
+    
+    [sig_signal1]
+    ...
+    
+    [src_src1]
+    ...
+    
+    [src_src2]
+    ...
+    
+    [proc_src1_signal1]
+    ...
+    
+    [proc_src2_signal1]
+    ...
+
 """
 
 from configparser import ConfigParser, RawConfigParser
@@ -34,9 +69,67 @@ class DirectSingleConfig(ConfigParser):
         for i, section in enumerate(sections):
             print("%d: %s" % (i, section))
         idx = int(input("Select section: "))
-        self.loadSection(sections[idx])         
+        self.loadSection(sections[idx])
 
-#%% 
+#%%
+class SourceConfigMixin:
+    '''This mixin contains the parameters needed to read the samples.'''
+    def loadSrc(self, src: str):
+        '''This looks for a section prefixed with "src_" and loads it.'''
+        self.loadSection('src_' + src)
+    
+    @property
+    def srcdir(self):
+        return self.currentSection.get('srcdir')
+    
+    @property
+    def fs(self):
+        return self.currentSection.getfloat('fs')
+    
+    @property
+    def fc(self):
+        return self.currentSection.getfloat('fc')
+    
+    @property
+    def conjSamples(self):
+        return self.currentSection.getbool('conjSamples')
+    
+    @property
+    def headerBytes(self):
+        return self.currentSection.getint('headerBytes')
+    
+    @property
+    def dtype(self):
+        return self.currentSection.get('dtype')
+    
+#%%
+class SignalConfigMixin:
+    '''This mixin contains parameters related to a single target signal in the samples, and how to process it (for demodulation etc.).'''
+    def loadSig(self, sig: str):
+        '''This looks for a section prefixed with "sig_" and loads it.'''
+        self.loadSection('sig_' + sig)
+    
+    @property
+    def target_fc(self):
+        return self.currentSection.getfloat('target_fc')
+    
+    @property
+    def freqshift(self):
+        return self.currentSection.getfloat('freqshift')
+    
+    @property
+    def baud(self):
+        return self.currentSection.getfloat('baud')
+    
+    @property
+    def numTaps(self):
+        return self.currentSection.getint('numTaps')
+    
+    @property
+    def target_osr(self):
+        return self.currentSection.getint('target_osr')
+
+#%% Intend to deprecate from this onwards, bad format to do things..
 class SingleSourceConfigMixin:
     '''This mixin contains the parameters needed to read the samples.'''
     @property

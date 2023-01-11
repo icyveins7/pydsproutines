de910a14138167acd9f77ecc8faabd58b46648b3
lken <lken@dso.org.sg>
2023-01-11 18:20:24 +0800

    update czt benchmark

--- benchmarks/benchmark_czts.py
+++ benchmarks/benchmark_czts.py
@@ -8,6 +8,9 @@ Created on Wed Jan 11 17:44:12 2023
 if __name__ == "__main__":
     from spectralRoutines import *
     from signalCreationRoutines import *
+    from verifyRoutines import *
+    from timingRoutines import Timer
+    timer = Timer()
     
     length = 1000000
     f1 = -1000.0
@@ -15,10 +18,19 @@ if __name__ == "__main__":
     fstep = 0.001
     fs = 10000000
     
-    x = randnoise(length, 1, 1, 10).astype(np.complex64)
+    x = np.vstack([randnoise(length, 1, 1, 10).astype(np.complex64) for i in range(10)])
     
-    cztobj = CZTCachedGPU(length, f1, f2, fstep, fs)
+    d_cztobj = CZTCachedGPU(length, f1, f2, fstep, fs)
     d_x = cp.asarray(x)
     
-    out = cztobj.run(d_x)
-
+    timer.start()
+    d_out = d_cztobj.runMany(d_x)
+    timer.evt("czt gpu batch")
+    
+    #%%
+    cztobj = CZTCached(length, f1, f2, fstep, fs)
+    out = cztobj.runMany(x)
+    timer.evt("czt cpu batch")
+    
+    #%%
+    timer.end()

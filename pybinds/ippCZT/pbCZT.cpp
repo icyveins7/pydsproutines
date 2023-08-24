0f8cfbb74378d23733c64baa9f72a94127b5b92f
seoxubuntu <icyveins@hotmail.com>
2023-08-24 12:17:55 +0800

    correct binding code module name to fix setup error

--- pybinds/ippCZT/pbCZT.cpp
+++ pybinds/ippCZT/pbCZT.cpp
@@ -3,7 +3,9 @@
 
 namespace py = pybind11;
 
-PYBIND11_MODULE(pybindIppCZT32fc, m) {
+// This module name in PYBIND11_MODULE must be the same as the filename!
+// https://github.com/pybind/pybind11/blob/master/docs/faq.rst
+PYBIND11_MODULE(pbIppCZT32fc, m) {
     py::class_<IppCZT32fc>(m, "pbIppCZT32fc")
         .def(py::init<int, Ipp32f, Ipp32f, Ipp32f, Ipp32f>())
         .def("run", &IppCZT32fc::run)

20fe8c20c4bb2f87f0ee8a8657618bd5b1185111
seoxubuntu <icyveins@hotmail.com>
2023-08-31 20:03:48 +0800

    migrate pybinds setup.py to a single file on the outside
    this way we can use __init__.py on the subfolders to import IPP related
    things for us, similar to what we did with cython_ext

---
+++ pybinds/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+from pybind11.setup_helpers import Pybind11Extension, build_ext
+
+
+#%%
+ext_modules = [
+    Pybind11Extension(
+        "ippCZT.pbIppCZT32fc", # Using dot places it into the folder?
+        ["ippCZT/CZT.cpp", "ippCZT/pbCZT.cpp"],
+        include_dirs=["../ipp_ext/include"],
+        libraries=["ippcore", "ipps"],
+        # Define COMPILE_FOR_PYBIND
+        extra_compile_args=["-DCOMPILE_FOR_PYBIND"]
+    ),
+    Pybind11Extension(
+        "ippGroupXcorrCZT.pbIppGroupXcorrCZT",
+        ["ippCZT/CZT.cpp", 
+         "ippGroupXcorrCZT/GroupXcorrCZT.cpp", 
+         "ippGroupXcorrCZT/pbGroupXcorrCZT.cpp"],
+        include_dirs=["../ipp_ext/include"],
+        libraries=["ippcore", "ipps"],
+        # Define COMPILE_FOR_PYBIND
+        extra_compile_args=["-DCOMPILE_FOR_PYBIND"]
+    ),
+]
+
+setup(
+    name="pydspPybinds", 
+    ext_modules=ext_modules,
+    cmdclass={"build_ext": build_ext},
+)

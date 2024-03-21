b8a3fae69f4fe5c6a7dcf27870e2d063a22a4c5f
seo <icyveins@hotmail.com>
2024-03-21 13:30:41 +0800

    add reshaped small rows cupy fft to the benchmark

--- benchmarks/benchmark_cupyffts.py
+++ benchmarks/benchmark_cupyffts.py
@@ -2,15 +2,20 @@ import cupy as cp
 
 from timingRoutines import Timer
 
-length = 1000000
+length = 10000000
 data = cp.random.randn(length*2).astype(cp.float64).view(cp.complex128).astype(cp.complex64)
-# out = cp.zeros_like(data)
 
 loops = 2
 timer = Timer()
 timer.start()
 for i in range(loops):
     out = cp.fft.fft(data)
+
+# Then compare time to running many small FFTs
+smalllen = 200
+rsdata = data.reshape((-1,200))
+for i in range(loops):
+    out = cp.fft.fft(rsdata, axis=1) 
 
 total = timer.end()
 print("%fs per loop" % (total/loops))

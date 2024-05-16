3381240d87bee6586fce36f16c0cf047dba84406
icyveins7 <icyveins@hotmail.com>
2024-05-16 15:32:04 +0800

    put the proper things into the init.py files

---
+++ cython_ext/PyViterbiDemodulator/__init__.py
@@ -0,0 +1,9 @@
+import os
+if os.name == 'nt': # Load the directory on windows
+    os.add_dll_directory(os.path.join(os.environ['IPPROOT'], 'redist', 'intel64')) # Configure IPP dll reliance
+
+# Import the cythonized pyd directly into namespace
+try:
+    from .PyViterbiDemodulator import PyViterbiDemodulator # Don't forget the . because we are name mangling with the folder
+except Exception as e:
+    print("Cythonised module PyViterbiDemodulator not found. Please compile it: %s" % str(e))

016a0fa6eaf226e347b5eed47a6e6d4ad2125e06
seomac <icyveins@hotmail.com>
2024-05-24 11:38:04 +0800

    more updates to matrixprofiles with some tests

--- matrixProfileRoutines.py
+++ matrixProfileRoutines.py
@@ -4,11 +4,11 @@ import numpy as np
 class MatrixProfile:
     def __init__(
         self,
-        windowLength: int = 10,
+        windowLength: int,
         onlyUpperTriangle: bool = True
     ):
         self._windowLength = windowLength
-        self._norms = None
+        self._normsSq = None
 
     def compute(self, x: np.ndarray) -> np.ndarray:
         """
@@ -24,14 +24,28 @@ class MatrixProfile:
         mp : np.ndarray
             The matrix profile.
         """
-        self._computeNorms(x)
+        self._normsSq = None  # Reset values
+        self._normsSq = self._computeNorms(x)
 
-        raise NotImplementedError()
+    def _computeNormsSq(self, x: np.ndarray) -> np.ndarray:
+        """
+        Computes the moving window energy of the input,
+        defined as norm(x[n:n+windowLength])**2.
 
-    def _computeNorms(self, x: np.ndarray) -> np.ndarray:
+        Parameters
+        ----------
+        x : np.ndarray, length N
+            Input array.
+
+        Returns
+        -------
+        normsSq : np.ndarray, length N-windowLength+1
+            The moving window energy.
+        """
         power = np.abs(x)**2
-        self._norms = np.convolve(
-            ampl, np.ones(self._windowLength), mode='valid')
+        normsSq = np.convolve(
+            power, np.ones(self._windowLength), mode='valid')
+        return normsSq
 
     def _computeDiagonal(
         self,
@@ -59,27 +73,100 @@ class MatrixProfile:
 
         slice1 = x[0:-diagIdx]
         slice2 = x[diagIdx:]
+        if slice1.size <= 0 or slice2.size <= 0:
+            raise RuntimeError("Nothing in the diagonal.")
+
+        energy1 = self._normsSq[0:-diagIdx]
+        energy2 = self._normsSq[diagIdx:]
+        if energy1.size <= 0 or energy2.size <= 0:
+            raise RuntimeError("Indexing out of valid normSq values.")
 
         pdt = slice1 * slice2.conj()
         kdiag = np.convolve(pdt, np.ones(self._windowLength), mode='valid')
+        kdiag = np.abs(kdiag)**2
+        # Normalise appropriately
+        kdiag = kdiag / energy1 / energy2
+
         return kdiag
 
 
 if __name__ == "__main__":
-    from plotRoutines import *
+    # The following are used for unittests and signal gen
+    import unittest
+    from xcorrRoutines import calcQF2
     from signalCreationRoutines import randPSKsyms, addManySigToNoise
 
-    syms, bits = randPSKsyms(10, 4, dtype=np.complex64)
+    # These are used for example code and plotting only
+    import pyqtgraph as pg
+    from plotRoutines import pgPlotAmpTime, closeAllFigs
+    closeAllFigs()
+
+    windowLength = 10
+    numCopies = 3
+    syms, bits = randPSKsyms(windowLength, 4, dtype=np.complex64)
     noise, x = addManySigToNoise(
-        500,
-        np.arange(5) * 20,
-        [syms for i in range(5)],
-        1, 1, [10 for i in range(5)]
+        200,
+        np.arange(numCopies) * windowLength * 3,
+        [syms for i in range(numCopies)],
+        1, 1, [40.0 for i in range(numCopies)]
     )
 
-    mpo = MatrixProfile()
-    diag0 = mpo._computeDiagonal(x, 1)
+    mpo = MatrixProfile(windowLength)
+    mpo._normsSq = mpo._computeNormsSq(x)
+    k = 30
+    diag0 = mpo._computeDiagonal(x, k)
     print(diag0)
     print(diag0.shape)
 
-    pgPlotAmpTime(diag0)
+    awin, aax = pgPlotAmpTime(x, title='ampl-time')
+
+    win = pg.plot(np.arange(diag0.size) + k, diag0)
+    win.setTitle('Diagonal %d' % k)
+    win.show()
+
+    # Unittest code
+    class TestMatrixProfile(unittest.TestCase):
+        def setUp(self):
+            self.totalLength = 200
+            self.windowLength = 10
+            self.numCopies = 3
+            self.syms, self.bits = randPSKsyms(
+                windowLength, 4, dtype=np.complex64)
+            noise, self.x = addManySigToNoise(
+                self.totalLength,
+                np.arange(self.numCopies) * self.windowLength * 3,
+                [self.syms for i in range(self.numCopies)],
+                1, 1, [40.0 for i in range(self.numCopies)]
+            )
+
+            self.mpo = MatrixProfile(self.windowLength)
+
+        def test_computeNormsSq(self):
+            normsSq = self.mpo._computeNormsSq(self.x)
+            # Check that the length is correct
+            self.assertEqual(
+                normsSq.size,
+                self.totalLength - self.windowLength + 1)
+
+            # Check all values are correct
+            for i, nsq in enumerate(normsSq):
+                np.testing.assert_allclose(
+                    nsq,
+                    np.linalg.norm(self.x[i:i+self.windowLength])**2
+                )
+
+        def test_computeDiagonal(self):
+            # Here we manually set normsSq for testing purposes
+            self.mpo._normsSq = self.mpo._computeNormsSq(x)
+            for k in range(1, self.totalLength):
+                diag0 = mpo._computeDiagonal(x, k)
+                for i, dv in enumerate(diag0):
+                    np.testing.assert_allclose(
+                        dv,
+                        calcQF2(
+                            self.x[i:i+self.windowLength],
+                            self.x[i+k:i+k+self.windowLength]
+                        )
+                    )
+
+    unittest.main()

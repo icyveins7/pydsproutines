9150b2359b7caad82cfb17b7cfd83feaca46d104
unknown <icyveins@hotmail.com>
2024-05-29 14:49:00 +0800

    add some code to benchmark matrix profile without vram bottleneck

--- benchmarks/benchmark_matrixprofiles.py
+++ benchmarks/benchmark_matrixprofiles.py
@@ -16,14 +16,27 @@ d_x = cp.asarray(x)
 # Compute the matrix profile
 timer = Timer()
 
-timer.start()
-mpl = mp.compute(d_x)
-timer.end()
-
-print("Complete.")
-
 """
 Some stats for 100e3 length, 10 windowLength:
 Internal python timer: 18s.
 nsys profile: 22s.
+Note that this completely pummels the VRAM, so it gets throttled
+when it starts to run out of memory and virtualises some of it to the CPU.
 """
+# timer.start()
+# mpl = mp.compute(d_x)
+# timer.end()
+
+"""
+Benchmarking like this prevents the storage of the outputs, but still
+performs the compute.
+Some stats for 100e3 length, 10 windowLength:
+Internal python timer: 9s.
+"""
+timer.start()
+mp._normsSq = mp._computeNormsSq(d_x)
+for i in range(1, x.size - mp._windowLength + 1):
+    diag = mp._computeDiagonal(d_x, i)
+timer.end()
+
+print("Complete.")
